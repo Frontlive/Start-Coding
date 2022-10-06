c3ac17b8d4d2ae93a8067702ef9b3591273719e5
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2022-10-06 20:51:42 +0200

    WORKS

--- apps/api/config.ts
+++ apps/api/config.ts
@@ -1,8 +1,11 @@
-import { cleanEnv, num } from 'envalid';
+import { cleanEnv, num, str } from 'envalid';
 import { default as dotenv } from 'dotenv';
 
 dotenv.config();
 
 export const env = cleanEnv(process.env, {
 	PORT: num(),
+	DATABASE_URL: str(),
+	AUTH0_DOMAIN: str(),
+	BASE_URL: str(),
 });
--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,8 @@
-import { createServer } from '@graphql-yoga/node';
-import { makeSchema, queryField } from 'nexus';
+import { useAuth0 } from '@envelop/auth0';
+import { createYoga } from 'graphql-yoga';
+import { createApp } from 'h3';
+import { createServer } from 'http';
+import { makeSchema, objectType, queryField } from 'nexus';
 import { env } from './config';
 import { logger } from './logger';
 
@@ -10,6 +13,21 @@ const helloWorld = queryField('helloWorld', {
 	},
 });
 
+const authInfoType = objectType({
+	name: 'auth0',
+	definition(t) {
+		t.string('sub');
+	},
+});
+
+const authInfo = queryField('authInfo', {
+	type: authInfoType,
+	resolve(root, args, ctx) {
+		console.log('AUTH0', ctx.auth0);
+		return ctx.auth0;
+	},
+});
+
 const schema = makeSchema({
 	types: [helloWorld],
 	outputs: {
@@ -17,16 +35,33 @@ const schema = makeSchema({
 	},
 });
 
-const server = createServer({
-	port: env.PORT,
+const instance = createYoga({
 	schema,
+	plugins: [
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		useAuth0({
+			domain: env.AUTH0_DOMAIN,
+			audience: `https://${env.BASE_URL}/graphql`,
+			extendContextField: 'auth0',
+			preventUnauthenticatedAccess: true,
+			onError(error) {
+				console.log(error.message);
+			},
+		}),
+	],
 });
+
+const app = createApp();
+
+app.use('/graphql', instance);
+
+const server = createServer(app);
 
 const main = async () => {
 	try {
-		await server.start();
-
-		logger.success(`Server is running on port ${env.PORT}`);
+		server.listen(env.PORT, () => {
+			logger.success(`Server is running on port ${env.PORT}`);
+		});
 	} catch (err) {
 		logger.error(err);
 
