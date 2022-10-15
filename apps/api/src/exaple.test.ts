91688a2fd3d91a1b7786fd823e5b2a4a85af57ed
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2022-10-15 23:55:22 +0200

    Add test command and example test using experimental node:test

---
+++ apps/api/src/exaple.test.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+describe('Example test using native Node.js module', () => {
+	it('should pass', () => {
+		assert.equal(1, 1);
+	});
+});
