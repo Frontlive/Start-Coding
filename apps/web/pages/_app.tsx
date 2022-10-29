dcf2a37d1d634b9a14d1ebab01335c5839d9ca2d
survikrowa <szymonhrabia60@gmail.com>
2022-10-30 00:15:24 +0200

    feat/implement-get-layout

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,17 +1,24 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
-import { graphClient } from '../graphql/apolloClient';
-import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ApolloProvider client={graphClient}>
-			<UserProvider>
-				<Component {...pageProps} />
-			</UserProvider>
-		</ApolloProvider>
-	);
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
+
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
+	P,
+	IP
+> & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => page);
+
+	return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp;
