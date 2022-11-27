b19d8a5a88c1a6aefade5c9baf036458f6954f99
CubeStorm <cubestorm72@gmail.com>
2022-11-27 12:41:19 +0100

    feat/add heading component

---
+++ apps/web/components/atoms/heading/heading.tsx
@@ -0,0 +1,31 @@
+import type { HTMLAttributes, ReactNode } from 'react';
+import cx from 'clsx';
+import type { TextSize } from 'atoms/text/text';
+
+export type HeadingTag = keyof Pick<
+	JSX.IntrinsicElements,
+	'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+>;
+
+type HeadingProps = {
+	children: ReactNode;
+	tag: HeadingTag;
+	size: TextSize;
+} & HTMLAttributes<HTMLHeadingElement>;
+
+export const Heading = ({ children, tag, size, ...rest }: HeadingProps) => {
+	const Tag = tag;
+
+	return (
+		<Tag
+			className={cx({
+				'text-sm': size === 'small',
+				'text-base': size === 'medium',
+				'text-lg': size === 'large',
+			})}
+			{...rest}
+		>
+			{children}
+		</Tag>
+	);
+};
