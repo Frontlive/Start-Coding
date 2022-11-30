efcdb80d26f44fad2f438c603ff4cc84e7dc14d1
survikrowa <szymonhrabia60@gmail.com>
2022-11-30 18:32:49 +0100

    feat/begin-work-on-RICH-editor

---
+++ apps/web/components/molecules/editor/editor.tsx
@@ -0,0 +1,71 @@
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { EditorState, EditorThemeClasses, LexicalEditor } from 'lexical';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useEffect } from 'react';
+import {
+	registerCodeHighlighting,
+	CodeNode,
+	CodeHighlightNode,
+} from '@lexical/code';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { $generateHtmlFromNodes } from '@lexical/html';
+import { EditorToolbarPlugin } from 'molecules/editor/plugins/editorToolbar';
+import { Text } from 'atoms/text/text';
+
+type EditorProps = {
+	initialEditorState?: string;
+	onChange: (state: string) => void;
+};
+
+const CodeHighlightPlugin = () => {
+	const [editor] = useLexicalComposerContext();
+	useEffect(() => {
+		return registerCodeHighlighting(editor);
+	}, [editor]);
+	return null;
+};
+
+export const Editor = ({ initialEditorState, onChange }: EditorProps) => {
+	const handleChange = (editorState: EditorState, editor: LexicalEditor) => {
+		editor.update(() => {
+			const html = $generateHtmlFromNodes(editor, null);
+			onChange(html);
+		});
+	};
+	const theme: EditorThemeClasses = {};
+	const onError = (error: Error) => console.log(error);
+	const initialConfig = {
+		namespace: 'MyEditor',
+		theme,
+		onError,
+		nodes: [CodeNode, CodeHighlightNode],
+		editorState: initialEditorState,
+	};
+	return (
+		<>
+			<LexicalComposer initialConfig={initialConfig}>
+				<div className="mt-1 mb-2">
+					<Text size="medium" variant="default" tag="p" position="left">
+						Opis
+					</Text>
+				</div>
+
+				<EditorToolbarPlugin />
+				<RichTextPlugin
+					placeholder=""
+					contentEditable={
+						<ContentEditable className="p-2 mt-2 border-2 rounded-md min-h-100" />
+					}
+					ErrorBoundary={LexicalErrorBoundary}
+				/>
+				<OnChangePlugin onChange={handleChange} />
+				<HistoryPlugin />
+				<CodeHighlightPlugin />
+			</LexicalComposer>
+		</>
+	);
+};
