cf076d9177796fb27c4d7e7d0c1da456672a6a5e
Stanisław Synowiec <stasyn1410@gmail.com>
2022-12-23 17:35:14 +0100

    Feat/add Website of all tasks

---
+++ apps/web/components/templates/challenges/all/allChallenges.tsx
@@ -0,0 +1,32 @@
+export const AllChallenges = () => {
+	return <h1>Strona z zadaniami</h1>;
+};
+
+// type categoryOption = {
+// 	label: string;
+// 	value: string;
+// 	category: string;
+// };
+//
+// type FilterCategoryProps = {
+// 	categoryName: string;
+// 	categoryOptions: categoryOption[];
+// };
+//
+// import { FilterCategory } from 'organisms/filterCategory/filterCategory';
+//
+// const FilterCategories = ['lvl', 'tech', 'ocena'];
+//
+// const Filters: categoryOption[] = [
+// 	{ label: 'medium', value: 'medium', category: 'lvl' },
+// ];
+//
+// export const Homepage = () => {
+// 	return (
+// 		<>
+// 			{FilterCategories.map((category) => (
+// 				<FilterCategory categoryName={category} categoryOptions={Filters} />
+// 			))}
+// 		</>
+// 	);
+// };
