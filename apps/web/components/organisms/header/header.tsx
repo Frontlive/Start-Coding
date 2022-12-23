5385619cddfafc52d43316679a18a27c57478d17
Stanisław Synowiec <stasyn1410@gmail.com>
2022-12-23 17:35:14 +0100

    Feat/add Website of all tasks

--- apps/web/components/organisms/header/header.tsx
+++ apps/web/components/organisms/header/header.tsx
@@ -15,9 +15,11 @@ export const Header = () => {
 		useHamburgerMenu();
 	return (
 		<header className="flex  px-12 py-4  justify-between items-center w-full sticky shadow-xl z-10">
-			<h1 className="font-bold text-2xl whitespace-nowrap	 mr-20">
-				Start-Coding
-			</h1>
+			<a href="/" className="no-underline">
+				<h1 className="font-bold text-2xl whitespace-nowrap	 mr-20">
+					Start-Coding
+				</h1>
+			</a>
 
 			<button onClick={openBurgerMenu} className="lg:hidden">
 				<Image src={BurgerIcon} alt="" height={50} width={50} />
@@ -26,14 +28,14 @@ export const Header = () => {
 			{isBurgerMenuActive && (
 				<DropDownBurgerMenu hideBurgerMenu={closeBurgerMenu}>
 					<Navbar>
-						<NavLink link={{ name: 'Zadania', href: '' }} />
+						<NavLink link={{ name: 'Zadania', href: '/challenges' }} />
 					</Navbar>
 				</DropDownBurgerMenu>
 			)}
 			<div className="hidden lg:flex items-center justify-between w-full">
 				<div className="flex items-center justify-between max-w-sm w-full">
 					<Navbar>
-						<NavLink link={{ name: 'Zadania', href: '' }} />
+						<NavLink link={{ name: 'Zadania', href: '/challenges' }} />
 					</Navbar>
 				</div>
 				{!isLoading && !user && <HeaderUserLoginContent />}
