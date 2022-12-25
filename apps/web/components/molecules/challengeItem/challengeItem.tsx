b6b99e8a1c5f18f8b73af3ddbb551d23eba81a77
Stanisław Synowiec <stasyn1410@gmail.com>
2022-12-25 20:24:04 +0100

    Feat/fix list of challenges as ul list

--- apps/web/components/molecules/challengeItem/challengeItem.tsx
+++ apps/web/components/molecules/challengeItem/challengeItem.tsx
@@ -6,30 +6,32 @@ type ChallengeItemProps = {
 };
 export const ChallengeItem = ({ challenge }: ChallengeItemProps) => {
 	return (
-		<a
-			href="/challenge/id"
-			className="block mb-3 hover:scale-105 transition-transform"
-		>
-			<Card tag="div">
-				<div className="flex flex-row">
-					<img src={challenge.image} alt="" width="250px" />
-					<div className="ml-4">
-						<h2 className="font-bold">{challenge.title}</h2>
-						<p>{challenge.description}</p>
-						<div className="py-2">
-							<p>
-								<span>📶 </span>
-								{challenge.difficulty}
-							</p>
-							<p>
-								<span>⭐️⭐️⭐️⭐️⭐️ </span>
-								4.8/5
-								{/*{challenge.rating}*/}
-							</p>
+		<li>
+			<a
+				href="/challenge/id"
+				className="block mb-3 hover:scale-105 transition-transform"
+			>
+				<Card tag="div">
+					<div className="flex flex-row">
+						<img src={challenge.image} alt="" width="250px" />
+						<div className="ml-4">
+							<h2 className="font-bold">{challenge.title}</h2>
+							<p>{challenge.description}</p>
+							<div className="py-2">
+								<p>
+									<span>📶 </span>
+									{challenge.difficulty}
+								</p>
+								<p>
+									<span>⭐️⭐️⭐️⭐️⭐️ </span>
+									4.8/5
+									{/*{challenge.rating}*/}
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-			</Card>
-		</a>
+				</Card>
+			</a>
+		</li>
 	);
 };
--- apps/web/components/templates/challenges/all/allChallenges.tsx
+++ apps/web/components/templates/challenges/all/allChallenges.tsx
@@ -3,6 +3,7 @@ import { Challenge } from '../../../../types/types';
 
 const challenges: Challenge[] = [
 	{
+		id: 1,
 		title: 'First Challenge',
 		description:
 			'This is example of challenge. In hac habitasse platea dictumst. Praesent eu auctor velit. Cras mattis gravida odio euismod placerat. Morbi vestibulum dapibus diam, a lacinia felis porttitor vel. Ut sodales tincidunt orci, vel condimentum nibh. Fusce in elit euismod, laoreet felis vel, rutrum lacus.',
@@ -12,6 +13,7 @@ const challenges: Challenge[] = [
 			'https://images.unsplash.com/photo-1498008122250-bcb854c8462d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 	},
 	{
+		id: 2,
 		title: 'Second Challenge',
 		description:
 			'This is example of challenge. In hac habitasse platea dictumst. Praesent eu auctor velit. Cras mattis gravida odio euismod placerat. Morbi vestibulum dapibus diam, a lacinia felis porttitor vel. Ut sodales tincidunt orci, vel condimentum nibh. Fusce in elit euismod, laoreet felis vel, rutrum lacus.',
@@ -25,11 +27,13 @@ const challenges: Challenge[] = [
 
 export const AllChallenges = () => {
 	return (
-		<div className="mx-auto w-11/12">
-			<h1>Strona z zadaniami</h1>
-			{challenges.map((challenge) => (
-				<ChallengeItem challenge={challenge} />
-			))}
-		</div>
+		<section className="mx-auto w-11/12" aria-labelledby="pageHeading">
+			<h1 id="pageHeading">Strona z zadaniami</h1>
+			<ul>
+				{challenges.map((challenge) => (
+					<ChallengeItem key={challenge.id} challenge={challenge} />
+				))}
+			</ul>
+		</section>
 	);
 };
