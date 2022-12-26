951d57c3ecd68f567e779b0c458eeeb228c11470
kamiloox <kamiltroczewski@gmail.com>
2022-12-26 15:09:31 +0100

    feat(ui): add tailwindcss

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line no-undef
 module.exports = {
 	root: true,
 	extends: ['custom'],
+	env: {
+		node: true,
+	},
 };
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,1 +1,3 @@
 export const add = () => 2 * 2;
+
+export const remove = () => 2 * 3;
