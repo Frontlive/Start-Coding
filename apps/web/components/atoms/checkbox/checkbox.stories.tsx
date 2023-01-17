aed78a1dd5b50f66779c7956e3217557f0729e8d
Stanisław Synowiec <stasyn1410@gmail.com>
2023-01-17 14:09:25 +0100

    feat/add: Checkbox component

---
+++ apps/web/components/atoms/checkbox/checkbox.stories.tsx
@@ -0,0 +1,17 @@
+import { Checkbox } from 'atoms/checkbox/checkbox';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+    title: 'Main Library/atoms/Checkbox',
+    component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+export const Primary: ComponentStory<typeof Checkbox> = (props) => {
+    return (
+        <Checkbox {...props}></Checkbox>
+    );
+};
+
+Primary.args = {
+    label: 'checkbox',
+};
