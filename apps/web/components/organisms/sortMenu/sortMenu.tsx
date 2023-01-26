189f9a54cfc02da85f8deee449d61f138be44fb0
Stanisław Synowiec <stasyn1410@gmail.com>
2023-01-26 22:40:47 +0100

    feat/add: sort menu

--- apps/web/components/organisms/sortMenu/sortMenu.tsx
+++ apps/web/components/organisms/sortMenu/sortMenu.tsx
@@ -13,7 +13,7 @@ export const SortMenu = ({ sortOptions }: SortMenuProps) => {
 		<Menu as="div" className="relative inline-block text-left">
 			<div>
 				<Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-					Sort
+					Sortuj
 					<ChevronDownIcon
 						className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
 						aria-hidden="true"
