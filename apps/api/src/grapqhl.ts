cb78b19b76c111c42ed6ded29a07e92af1d48232
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-01-31 19:35:20 +0100

    Feat/move general things from previous PR (#66)

--- apps/api/src/grapqhl.ts
+++ apps/api/src/grapqhl.ts
@@ -1,11 +1,13 @@
 import { useAuth0 } from '@envelop/auth0';
 import { EnvelopArmorPlugin } from '@escape.tech/graphql-armor';
 import { pipe } from '@mobily/ts-belt';
+import type { PrismaClient } from '@prisma/client';
 import { lexicographicSortSchema, printSchema } from 'graphql';
 import { rateLimitDirective } from 'graphql-rate-limit-directive';
-import { createYoga } from 'graphql-yoga';
+import { createYoga, useExtendContext } from 'graphql-yoga';
 
 import { env } from './config';
+import { client } from './prisma';
 import { schema as rawSchema } from './schema';
 import type { Context } from './types';
 
@@ -23,12 +25,29 @@ export const instance = createYoga<Context>({
 	schema,
 	maskedErrors: env.isProd,
 	landingPage: env.isDev,
+	graphiql: {
+		headers: JSON.stringify({
+			Authorization: 'Bearer <token>',
+		}),
+	},
+	context: (ctx) => ({
+		...ctx,
+		db: client,
+	}),
 	plugins: [
 		useAuth0({
 			domain: env.AUTH0_DOMAIN,
-			audience: `https://${env.BASE_URL}/graphql`,
+			audience: `${env.BASE_URL}/graphql`,
 			extendContextField: 'auth0',
 			preventUnauthenticatedAccess: env.isProd,
+		}),
+		useExtendContext(async (ctx: Context & { db: PrismaClient }) => {
+			const user_id = ctx.auth0?.sub;
+			const user = user_id
+				? await ctx.db.user.findUnique({ where: { user_id } })
+				: null;
+
+			return { ...ctx, user };
 		}),
 		EnvelopArmorPlugin({
 			maxDepth: { n: 5 },
