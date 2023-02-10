439d809f93c09ebea1c5b9311994c07bf93a962d
Stanisław Synowiec <stasyn1410@gmail.com>
2023-02-10 15:25:20 +0100

    icons

--- apps/web/components/molecules/challengeItem/challengeItem.tsx
+++ apps/web/components/molecules/challengeItem/challengeItem.tsx
@@ -1,4 +1,6 @@
 import { Card, Heading, Text, CategoryTag } from 'ui';
+import { ChartBarIcon, StarIcon as StarFill } from '@heroicons/react/20/solid';
+import { StarIcon as Star } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import type { Challenge } from '../../../types/types';
 
@@ -11,6 +13,7 @@ export const ChallengeItem = ({
 	description,
 	difficulty,
 	tags,
+	rating,
 }: TaskItemProps) => {
 	return (
 		<li className="mb-5">
@@ -25,7 +28,7 @@ export const ChallengeItem = ({
 							className="w-full h-full rounded-lg"
 						/>
 						<div className="lg:px-5">
-							<Heading tag="h2" size="large">
+							<Heading tag="h2" size="large" className="font-bold	">
 								{title}
 							</Heading>
 							<Text size="medium" variant="default" tag="p" position="left">
@@ -33,9 +36,17 @@ export const ChallengeItem = ({
 							</Text>
 						</div>
 					</div>
-					<Text size={'medium'} variant="default" tag="p" position="left">
-						📶 {difficulty}
-					</Text>
+					<div className="flex items-center">
+						<ChartBarIcon className="h-5 w-5 mr-1" />
+						<Text size="medium" variant="default" tag="p" position="left">
+							{difficulty}
+						</Text>
+						<StarFill className="h-5 w-5 ml-1" />
+						<Star className="h-5 w-5" />
+						<Text size="medium" variant="default" tag="p">
+							{rating}/5
+						</Text>
+					</div>
 					<ul className="flex flex-wrap">
 						{tags.map((tag) => {
 							return <CategoryTag key={tag} label={tag} />;
