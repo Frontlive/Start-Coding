7a783bce78742101f40a8e2f82884e562ec327eb
Szymon Hrabia <35381167+Survikrowa@users.noreply.github.com>
2023-02-23 12:06:57 +0100

    feat/loader-component (#79)
    * feat/loader-component
    
    * fix/storybook-deploy

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -20,4 +20,21 @@ module.exports = {
 	core: {
 		builder: '@storybook/builder-webpack5',
 	},
+	webpackFinal: async (config) => {
+		const imageRule = config.module.rules
+			.filter((rule) => Object.keys(rule).length !== 0)
+			.find((rule) => {
+				console.log(rule);
+				return rule.test.test('.svg');
+			});
+		imageRule.exclude = /\.svg$/;
+
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack', 'url-loader'],
+		});
+
+		return config;
+	},
 };
