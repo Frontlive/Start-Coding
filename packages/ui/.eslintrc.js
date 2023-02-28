bc139d8e6d95e9fce9d54cd52d5a3357a4498b72
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-03-01 00:39:36 +0100

    Revert "fix: format code with prettier"
    This reverts commit 2c7105a7932566d1870225ea00cdbbbb08bf50aa.

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	root: true,
-	extends: ['custom', 'plugin:storybook/recommended'],
-	env: {
-		node: true,
-	},
+  root: true,
+  extends: ['custom', 'plugin:storybook/recommended'],
+  env: {
+    node: true
+  }
 };
