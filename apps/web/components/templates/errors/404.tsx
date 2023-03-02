f100c26207d65fbd7ef88fab0dc2bafea4884a97
Stanisław Synowiec <stasyn1410@gmail.com>
2023-03-02 14:47:24 +0100

    style: 404 page

--- apps/web/components/templates/errors/404.tsx
+++ apps/web/components/templates/errors/404.tsx
@@ -2,16 +2,18 @@ import { Heading, Link, Text } from 'ui';
 
 export const Error404 = () => {
 	return (
-		<section className="flex flex-col justify-center items-center">
-			<Heading tag="h1" size="large">
+		<section className="flex h-[85vh] md:h-[90vh] flex-col justify-center items-center">
+			<Heading tag="h1" size="large" className="text-4xl pb-5">
 				Error 404
 			</Heading>
 			<Text size="medium" variant="default" tag="p">
 				Podana strona nie istnieje
 			</Text>
-			<Link href="/" variant="primary">
-				Wróć na stronę główną
-			</Link>
+			<div className="pt-5">
+				<Link href="/" variant="primary">
+					Wróć na stronę główną
+				</Link>
+			</div>
 		</section>
 	);
 };
