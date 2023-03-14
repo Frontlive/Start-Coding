275a3a6db7779d89a0aefdc234c4efa241cc77c6
Stanisław Synowiec <stasyn1410@gmail.com>
2023-03-14 18:17:11 +0100

    export TextArea

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -8,6 +8,7 @@ export * from './components/atoms/visuallyHidden/visuallyHidden';
 export * from './components/atoms/dropdownMenu/dropdownMenuItem/dropdownMenuItem';
 export * from './components/molecules/spinner/spinner';
 export * from './components/organisms/input/input';
+export * from './components/organisms/textArea/textArea';
 export * from './components/organisms/select/select';
 export * from './components/organisms/dropdownMenu/dropdownMenu';
 export * from './components/molecules/checkbox/checkbox';
