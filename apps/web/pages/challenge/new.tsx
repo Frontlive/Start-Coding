2ae883d1cfc63a4d42025e1ee689e335691b5a41
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-03-21 20:42:13 +0100

    Remove helper

--- apps/web/pages/challenge/new.tsx
+++ apps/web/pages/challenge/new.tsx
@@ -2,8 +2,6 @@ import { MainLayout } from 'layouts/mainLayout';
 import { NewChallengePage } from 'templates/challenge/new/newChallenge';
 import { ProtectedPage } from 'organisms/protectedPage/protectedPage';
 import type { NextPageWithLayout } from '../_app';
-import type { GetServerSideProps } from 'next';
-import { getSession } from '@auth0/nextjs-auth0';
 
 const NewChallenge: NextPageWithLayout = () => {
 	return (
@@ -18,15 +16,3 @@ NewChallenge.getLayout = function getLayout(page) {
 };
 
 export default NewChallenge;
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const session = getSession(ctx.req, ctx.res);
-
-	if (session) {
-		console.log(session.accessToken);
-	}
-
-	return {
-		props: {},
-	};
-};
