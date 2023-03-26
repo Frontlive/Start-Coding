fb6514aa3303ee080d91cd770ba8d83d2f7be092
Stanisław Synowiec <stasyn1410@gmail.com>
2023-03-26 22:26:29 +0200

    chore: fetch challenges from api

--- apps/web/components/organisms/challengesList/challengesList.tsx
+++ apps/web/components/organisms/challengesList/challengesList.tsx
@@ -1,9 +1,10 @@
 import { ChallengeItem } from 'molecules/challengeItem/challengeItem';
-import type { Challenge } from '../../../types/types';
+import type { GetTaskWithPaginationQueryHookResult } from 'generated';
 
 type TasksListProps = {
-	challenges: Challenge[];
+	challenges: GetTaskWithPaginationQueryHookResult['data']['tasks'][];
 };
+
 export const ChallengesList = ({ challenges }: TasksListProps) => {
 	return (
 		<div className="flex flex-col justify-between lg:col-span-3">
@@ -12,10 +13,6 @@ export const ChallengesList = ({ challenges }: TasksListProps) => {
 					return <ChallengeItem key={challenge.id} {...challenge} />;
 				})}
 			</ul>
-
-			{/*<div className="h-96 rounded-lg border-4 border-dashed border-gray-200 lg:h-full">*/}
-			{/*	Zadania*/}
-			{/*</div>*/}
 		</div>
 	);
 };
