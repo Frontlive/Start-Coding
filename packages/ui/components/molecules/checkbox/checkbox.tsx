a14b11898f0018e90d203377af18b30d3508e351
Stanisław Synowiec <stasyn1410@gmail.com>
2023-03-26 22:21:30 +0200

    fix: add default checked props

--- packages/ui/components/molecules/checkbox/checkbox.tsx
+++ packages/ui/components/molecules/checkbox/checkbox.tsx
@@ -2,9 +2,10 @@ import { useId } from 'react';
 
 type CheckboxProps = {
 	label: string;
+	checked: boolean;
 };
 
-export const Checkbox = ({ label }: CheckboxProps) => {
+export const Checkbox = ({ label, checked }: CheckboxProps) => {
 	const id = useId();
 	return (
 		<div key={id} className="flex items-center">
@@ -12,6 +13,7 @@ export const Checkbox = ({ label }: CheckboxProps) => {
 				id={id}
 				name={id}
 				type="checkbox"
+				checked={checked}
 				className="h-4 w-4 rounded-lg border-gray-200 text-indigo-600 dark:border-gray-300 focus:ring-indigo-500"
 			/>
 			<label htmlFor={id} className="ml-3 text-sm text-gray-600">
